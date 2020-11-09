66d78983096dc68a7e39c849f885344031c9a2e1
Milandou Nganga <56160689+cdjohnson97@users.noreply.github.com>
2020-11-09 18:59:25 +0100

    Avancements

--- app.js
+++ app.js
@@ -1,16 +1,46 @@
 const Person = {
     name: 'Johnson',
     prenom: 'Darlon',
-    age: '44ans'
+    age: '44ans',
+
+    familyMembers: [
+        { name: 'Mom', prenom: 'pulcherie', type: 'P' },
+        { name: 'Dad', prenom: 'Alfred', type: 'P' },
+        { name: 'Sis', prenom: 'Michka' },
+        { name: 'wife', prenom: 'Hora' },
+    ],
+
+    sayHi() {
+        console.log(`Hello my name is ${this.name} my father's name is ${this.familyMembers[1].prenom}`)
+    },
+
+    getFamilyMembers() {
+        return this.familyMembers
+    }
 }
 
 const { name, prenom: firstName, age } = Person
 
-const sayHi = (...args) => {
-    if (Person.name) {
-        console.log(`Hello my name is ${name} and my firstName is ${firstName}`)
 
-    }
+Person.sayHi()
+const family = Person.getFamilyMembers()
+
+
+family.forEach(member => {
+    console.log(member.name, member.prenom)
+})
+
+console.log('-----------------------------')
+const countMembers = (...args) => {
+    args.forEach(items => console.log(items.name, items.prenom))
 }
 
-sayHi([name, firstName])
+countMembers(...family)
+
+console.log('--------------Mom and Dad------------ ')
+
+family.map(momAndDad => {
+    if (momAndDad.type === 'P') {
+        console.log(momAndDad.prenom)
+    }
+})
