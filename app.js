bb154f179bb4ea523248081ba05e3769d2d216b7
Milandou Nganga <56160689+cdjohnson97@users.noreply.github.com>
2020-11-09 17:18:54 +0100

    Ajout d'un petit Script

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-</h1>Hello world !</h2>
+const Person =  {
+    name: 'Johnson',
+    prenom: 'Darlon',
+    age: '44ans'
+}
+
+const { name, firstName, age } = Person
+
+
+console.log(`Hello my name is ${name} nd my firstName is ${firstName}`)
